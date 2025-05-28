ac9a0d3bedda09c075e5725d7f5b5b60a998b332
Matt <matt.thornfield@neueda.com>
2025-05-28 12:42:32 +0000

    publisher v1

---
+++ src/main/java/com/neueda/expenses_management/kafka/EmployeePublisher.java
@@ -0,0 +1,18 @@
+package com.neueda.expenses_management.kafka;
+
+import com.neueda.expenses_management.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeePublisher {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendNewEmployeeMessage(Employee employee) {
+        kafkaTemplate.send("demo1", employee.getId().toString(), employee.toString());
+    }
+
+}
