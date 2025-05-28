d3e44b7eeca0d2112c3aa9bc088bdd368060e1be
Matt <matt.thornfield@neueda.com>
2025-05-28 13:48:41 +0000

    json for publishing

--- src/main/java/com/neueda/expenses_management/kafka/EmployeePublisher.java
+++ src/main/java/com/neueda/expenses_management/kafka/EmployeePublisher.java
@@ -1,5 +1,7 @@
 package com.neueda.expenses_management.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neueda.expenses_management.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -12,9 +14,14 @@ public class EmployeePublisher {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendNewEmployeeMessage(Employee employee) {
+        ObjectMapper om = new ObjectMapper();
+        om.findAndRegisterModules();
         //send (topic, key, value)
-        kafkaTemplate.send("demo1", employee.getId().toString(), employee.toString());
-
+        try {
+            kafkaTemplate.send("demo1", employee.getId().toString(), om.writeValueAsString(employee));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
